494d7bad0246b6f33d3151399dd33ca6833e6067
Roman Ovsepian <romaad2027@gmail.com>
2022-11-22 02:17:25 +0200

    Add husky pre-commit script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: 'standard',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ["sonarjs"],
-  extends: ["standard", "plugin:sonarjs/recommended", 'prettier'],
-  rules: {
-  }
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'standard',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['sonarjs'],
+    extends: ['standard', 'plugin:sonarjs/recommended', 'prettier'],
+    rules: {},
 }
--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
     const { name = 'World' } = res.query
-    return res.send('Hello ${name}!')
+    return res.send(`Hello ${name}!`)
 }
